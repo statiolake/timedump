7a771a5fbadb68efd9681fb87359947a7ac52e3f
statiolake <statiolake@gmail.com>
2019-07-07 20:34:58 +0900

    Implement recording

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use chrono::Local;
+use std::env;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+fn save_path() -> PathBuf {
+    env::current_exe()
+        .expect("Failed to get current exe's path.")
+        .with_file_name("did.txt")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let did = env::args().nth(1).expect("Please specify what you did.");
+    assert!(
+        !did.contains(','),
+        "You cannot include ',' to what you did."
+    );
+
+    let save_path = save_path();
+
+    let mut save_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(&save_path)
+        .unwrap_or_else(|_| panic!("Failed to open {}.", save_path.display()));
+
+    writeln!(save_file, "{},{}", Local::now().timestamp(), did)
+        .expect("Failed to write to the save file.");
 }
